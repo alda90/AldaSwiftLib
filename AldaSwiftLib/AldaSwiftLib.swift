73e5b64ce5f77f56a79a7160a087c55fa1a57045
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-18 16:33:11 -0700

    Added test cases

---
+++ AldaSwiftLib/AldaSwiftLib.swift
@@ -0,0 +1,21 @@
+//
+//  AldaSwiftLib.swift
+//  AldaSwiftLib
+//
+//  Created by Aldair Carrillo on 18/12/20.
+//  Copyright © 2020 Aldair Carrillo. All rights reserved.
+//
+
+public final class AldaSwiftLib {
+
+    let name = "AldaSwiftLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- AldaSwiftLibTests/AldaSwiftLibTests.swift
+++ AldaSwiftLibTests/AldaSwiftLibTests.swift
@@ -11,24 +11,19 @@ import XCTest
 
 class AldaSwiftLibTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var aldaSwiftLib: AldaSwiftLib!
+
+    override func setUp() {
+        aldaSwiftLib = AldaSwiftLib()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(aldaSwiftLib.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(aldaSwiftLib.sub(a: 2, b: 1), 1)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
 
 }
