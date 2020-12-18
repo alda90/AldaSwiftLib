fbad983c84f0dd08da46731adc55bd93bae83488
Aldair Carrillo <aldacdh904@gmail.com>
2020-12-18 16:02:33 -0700

    First Commit

---
+++ AldaSwiftLib/AldaSwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  AldaSwiftLib.h
+//  AldaSwiftLib
+//
+//  Created by Aldair Carrillo on 18/12/20.
+//  Copyright © 2020 Aldair Carrillo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AldaSwiftLib.
+FOUNDATION_EXPORT double AldaSwiftLibVersionNumber;
+
+//! Project version string for AldaSwiftLib.
+FOUNDATION_EXPORT const unsigned char AldaSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AldaSwiftLib/PublicHeader.h>
+
+
---
+++ AldaSwiftLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AldaSwiftLibExamples
+//
+//  Created by Aldair Carrillo on 18/12/20.
+//  Copyright © 2020 Aldair Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
